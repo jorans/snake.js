2232a222d43239e2f94eccbb911cce6a61ae09e8
Jöran Stark <joran.stark@uadm.uu.se>
2019-08-14 21:59:33 +0200

    Handle game state: setup/running/game over

--- src/games/snake/Snake.js
+++ src/games/snake/Snake.js
@@ -1,19 +1,19 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+
 
 function Snake(props) {
+    let SETUP = 0;
+    let RUNNING = 1;
+    let GAMEOVER = 2;
     let maxY = 50;
     let maxX = 100;
     let board = createBoard(maxY, maxX);
 
-    const [transform, setTransform] = useState({dX:0, dY:-1});
-    const [snake, setSnake] = useState([
-        {X:maxX/2, Y:maxY/2},
-        {X:maxX/2, Y:maxY/2},
-        {X:maxX/2, Y:maxY/2},
-        {X:maxX/2, Y:maxY/2}
-        ]);
+    const [gameState, setGameState] = useState(SETUP);
+    const [speed, setSpeed] = useState(getInitialSpeed());
+    const [transform, setTransform] = useState(getInitialTransformer());
+    const [snake, setSnake] = useState(getInitialSnakeState(maxX, maxY));
 
-    const [speed, setSpeed] = useState(1000);
     snake.forEach(p => board[p.Y].values[p.X].value = 1)
 
     let boardUI = board.map(row => {
@@ -22,7 +22,7 @@ function Snake(props) {
             return <td key={col.id} className={className}>&nbsp;</td>
 
         });
-        return(
+        return (
             <tr key={row.id}>
                 {cols}
             </tr>
@@ -39,37 +39,62 @@ function Snake(props) {
 
     function move(event) {
         var key = event.key || "";
+        if (gameState === RUNNING) {
+            if (key === "ArrowUp") {
+                let newSpeed = speed - 100;
+                console.log("MOVE - faster", newSpeed);
+                setSpeed(newSpeed);
+            } else if (key === "ArrowDown") {
+                console.log("MOVE - slower");
+                setSpeed(speed + 100);
+            } else if (key === "ArrowLeft") {
+                console.log("MOVE - turn left");
+                setTransform({dY: transform.dX * (-1), dX: transform.dY});
+            } else if (key === "ArrowRight") {
+                console.log("MOVE - turn right");
+                setTransform({dY: transform.dX, dX: transform.dY * (-1)});
+            } else {
+                console.log("MOVE - " + event.key);
 
-        if (key === "ArrowUp") {
-            let newSpeed = speed - 100;
-            console.log("MOVE - faster", newSpeed)
-            setSpeed(newSpeed);
-        } else if (key === "ArrowDown") {
-            console.log("MOVE - slower")
-            setSpeed(speed + 100);
-        } else if (key === "ArrowLeft") {
-            console.log("MOVE - turn left")
-            setTransform({dY: transform.dX*(-1), dX: transform.dY})
-        } else if (key === "ArrowRight") {
-            console.log("MOVE - turn right")
-            setTransform({dY: transform.dX, dX: transform.dY*(-1)})
-        } else {
-            console.log("MOVE - " + event.key)
-
+            }
         }
     }
 
-    useInterval(() => moveForward(), speed);
-    return(
-        <div className={"snake"} align={'center'}  onKeyDown={move} tabIndex={0}>
+    function handleOnClick(evt) {
+        console.log("startGame", gameState)
+        if (gameState === SETUP) {
+            setGameState(RUNNING);
+        } else if (gameState === RUNNING) {
+            setGameState(GAMEOVER);
+        } else if (gameState === GAMEOVER) {
+            setTransform(getInitialTransformer());
+            setSnake(getInitialSnakeState(maxX, maxY));
+            setSpeed(getInitialSpeed());
+            setGameState(SETUP);
+        }
+    }
+    useInterval(() => {if (gameState === RUNNING) moveForward()}, speed);
+
+    var info = (() => {
+        if (gameState === SETUP) {
+            return "Klick to start";
+        } else if (gameState === RUNNING) {
+            return "Use LEFT and RIGHT to turn the snake";
+        } else if (gameState === GAMEOVER) {
+            return "Game Over!"
+        }
+    })();
+
+    return (
+        <div className={"snake"} align={'center'} onClick={handleOnClick} onKeyDown={move} tabIndex={0}>
             <h1>Welcome to Snake</h1>
+            <p>{info}</p>
             <table className={"board"}>
                 <tbody>{boardUI}</tbody>
             </table>
         </div>
     )
 }
-
 function createBoard(height, width) {
     var result = [];
     for (var i = 0 ; i < height; i++) {
@@ -79,6 +104,20 @@ function createBoard(height, width) {
         }
     }
     return result;
+}
+function getInitialSnakeState(maxX, maxY) {
+    return [
+        {X: maxX / 2, Y: maxY / 2},
+        {X: maxX / 2, Y: maxY / 2},
+        {X: maxX / 2, Y: maxY / 2},
+        {X: maxX / 2, Y: maxY / 2}
+    ];
+}
+function getInitialTransformer() {
+    return {dX: 0, dY: -1};
+}
+function getInitialSpeed() {
+    return 1000;
 }
 
 function useInterval(callback, delay) {
