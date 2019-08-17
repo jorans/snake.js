6eaf174dc5fe80a7ce3237b4b9815544172023ae
Jöran Stark <joran.stark@uadm.uu.se>
2019-08-17 14:09:41 +0200

    Updates

---
+++ precache-manifest.83b55914605d89f044ccb5d132b623f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a240fd47e3ea8091fa43461448ae6385",
+    "url": "/snake.js/index.html"
+  },
+  {
+    "revision": "189445d8720a978995f9",
+    "url": "/snake.js/static/css/main.4627d054.chunk.css"
+  },
+  {
+    "revision": "b19e0e1ebfd1c1d7fe46",
+    "url": "/snake.js/static/js/2.f61800a5.chunk.js"
+  },
+  {
+    "revision": "189445d8720a978995f9",
+    "url": "/snake.js/static/js/main.fe0348e6.chunk.js"
+  },
+  {
+    "revision": "d779a8392a3d5ecf9981",
+    "url": "/snake.js/static/js/runtime~main.24f87462.js"
+  }
+]);
